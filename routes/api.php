bf7369703312cfc0469840a7a0a6167f9a485c71
Lucas Zambam <lucaszambam7896@gmail.com>
2025-09-21 18:18:35 -0300

    fix routes

--- routes/api.php
+++ routes/api.php
@@ -9,17 +9,24 @@ use Illuminate\Http\Request;
 use App\Models\Cidade;
 
 Route::post('/simulacoes', [SimulacaoApiController::class, 'store']);
+Route::get('/parametros', [ParametroApiController::class, 'index']);
 
 Route::get('/admin/login', [AuthController::class, 'showLoginForm'])->name('admin.login.form');
 Route::post('/admin/login', [AuthController::class, 'login'])->name('admin.login');
 Route::post('/admin/logout', [AuthController::class, 'logout'])->name('admin.logout');
 
-
-Route::get('/parametros', [ParametroController::class, 'index']);
-
 Route::get('/cidades', function (Request $request) {
-    $estadoId = $request->query('estado_id');
-    return Cidade::where('id_estado', $estadoId)
-                 ->orderBy('nome')
-                 ->get(['id_cidade','nome']);
+    $estadoId = $request->query('estado_id', false);
+    $estadoSigla = $request->query('estado_sigla', false);
+    if ($estadoId) {
+        return Cidade::where('id_estado', $estadoId)
+                     ->orderBy('nome')
+                     ->get(['id_cidade','nome']);
+    } else if ($estadoSigla) {
+        return Cidade::whereHas('estado', function ($q) use ($estadoSigla) {
+            $q->where('sigla', $estadoSigla);
+        })->orderBy('nome')->get(['id_cidade','nome']);
+    }
 });
+
+
